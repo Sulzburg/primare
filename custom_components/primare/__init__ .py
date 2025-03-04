0cc48df46eaa81aa27d7ebc8c34e04d6fb638208
GuidoL <86714721+Sulzburg@users.noreply.github.com>
2025-03-04 17:00:11 +0100

    Update __init__ .py

--- custom_components/primare/__init__ .py
+++ custom_components/primare/__init__ .py
@@ -1,9 +1,5 @@
-"""Init file for the Primare custom component."""
-
 import logging
-
 _LOGGER = logging.getLogger(__name__)
-
 def setup(hass, config):
     """Set up the Primare component."""
     _LOGGER.info("Primare Component wurde geladen.")
