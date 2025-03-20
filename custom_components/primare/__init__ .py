5230458cf14ee08cf72847cbdf7950c3aa03afec
GuidoL <86714721+Sulzburg@users.noreply.github.com>
2025-03-20 22:35:45 +0100

    Update __init__ .py

--- custom_components/primare/__init__ .py
+++ custom_components/primare/__init__ .py
@@ -1,10 +1,16 @@
-"""Init file for the Primare custom component."""
+from .coordinator import PrimareDataCoordinator
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
 
-import logging
+PLATFORMS = ["switch", "number", "select"]
 
-_LOGGER = logging.getLogger(__name__)
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    ip_address = config_entry.data["ip_address"]
+    device_name = config_entry.data["device_name"]
+    coordinator = PrimareDataCoordinator(ip_address, device_name, hass.loop)
+    await coordinator.async_start()
+    hass.data.setdefault("primare", {})["coordinator"] = coordinator
 
-def setup(hass, config):
-    """Set up the Primare component."""
-    _LOGGER.info("Primare Component wurde geladen.")
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+
     return True
