4ea9e1ad3fa3fc805a4430e1e70f1d99b3b81368
GuidoL <86714721+Sulzburg@users.noreply.github.com>
2025-03-25 00:02:52 +0100

    Update __init__ .py

--- custom_components/primare/__init__ .py
+++ custom_components/primare/__init__ .py
@@ -2,7 +2,7 @@ from .coordinator import PrimareDataCoordinator
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 
-PLATFORMS = ["switch", "number", "select"]
+PLATFORMS = ["switch", "number", "select", "media_player"]
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     ip_address = config_entry.data["ip_address"]
@@ -11,6 +11,6 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     await coordinator.async_start()
     hass.data.setdefault("primare", {})["coordinator"] = coordinator
 
+    # Leite alle in PLATFORMS definierten Plattformen weiter
     await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
-
     return True
