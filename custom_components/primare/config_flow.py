3e243bb1fc3850bebb68212a1f4ba33a4a916831
GuidoL <86714721+Sulzburg@users.noreply.github.com>
2025-05-02 09:36:12 +0200

    Update config_flow.py

--- custom_components/primare/config_flow.py
+++ custom_components/primare/config_flow.py
@@ -3,15 +3,21 @@ from homeassistant import config_entries
 
 DOMAIN = "primare"
 
+DEVICE_TYPE_OPTIONS = {
+    "SP25": "Multichannel",
+    "Pre35": "Stereo"
+}
+
 class PrimareConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         errors = {}
         if user_input is not None:
-            return self.async_create_entry(title="Primare", data=user_input)
+            return self.async_create_entry(title=user_input["device_name"], data=user_input)
         data_schema = vol.Schema({
             vol.Required("ip_address"): str,
             vol.Required("device_name"): str,
+            vol.Required("device_type", default="SP25"): vol.In(DEVICE_TYPE_OPTIONS),
         })
         return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
